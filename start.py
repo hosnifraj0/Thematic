408199c25f17a3ae8309bcc96c8326d485112fcd
Hosni <hosnifraj10@gmail.com>
2022-07-19 12:57:03 +0100

    feat: welcome message + refactor

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-
+## Using ANSI Code for color
 print("""\033[1;32m
 ,--------. ,--.                                    ,--.   ,--.        
 '--.  .--' |  ,---.   ,---.  ,--,--,--.  ,--,--. ,-'  '-. `--'  ,---. 
@@ -6,3 +6,9 @@ print("""\033[1;32m
    |  |    |  | |  | \   --. |  |  |  | \ '-'  |   |  |   |  | \ `--. 
    `--'    `--' `--'  `----' `--`--`--'  `--`--'   `--'   `--'  `---' 
 """)
+
+## Sparators 
+print("\n \n")
+
+# Print welcome message 
+print("\033[2;32m Thematic CLI is a themes setting tool which support ONLY Ubuntu for now.")
